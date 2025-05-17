a1113f959d3e408d44cb4fcba038619e0c573e4d
charan00000 <charan.koyaguri@gmail.com>
2025-05-17 04:02:44 -0400

    fix dowload_files_init.py

--- BackEnd/download_files_init.py
+++ BackEnd/download_files_init.py
@@ -2,14 +2,14 @@ import boto3
 import os
 from dotenv import load_dotenv
 
-os.makedirs('input', exist_ok=True)
+os.makedirs('BackEnd/input', exist_ok=True)
 load_dotenv()
 s3 = boto3.client('s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'), aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 #s3 = boto3.client('s3')
-files = s3.list_objects_v2(Bucket = 'tacticaiinternal', Prefix = 'input/nfl-big-data-bowl-2021')['Contents']
+files = s3.list_objects_v2(Bucket = 'tacticaiinternal', Prefix = 'input/')['Contents']
 
 for file in files:
-    s3.download_file('tacticaiinternal', file['Key'], file['Key'])
+    s3.download_file('tacticaiinternal', file['Key'], os.path.join('BackEnd', file['Key']))
 print('downloaded play data')
 
 
